aaa8a8e651f85477f4fa243028afecdfab57761e
Dave Simel <davesimel@gmail.com>
2015-08-01 17:47:31 -0400

    delete record

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :apartments, only: [:index, :show, :create, :update]
+      resources :apartments, only: [:index, :show, :create, :update, :destroy]
     end
   end
 end
